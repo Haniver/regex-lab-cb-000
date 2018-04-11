a52801a1fa4e8bd8b1cd8a8a92c04b52e1c615b5
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-11 22:44:56 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,11 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.scan(/\A[A-Z].*[,.;:]\z/) == []
-    false
-  else
-    true
-  end
+  text.match(/\A[A-Z].*[,.;:]\z/) ? true : false
 end
 
 def valid_phone_number?(phone)
